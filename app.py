d2d8d50cc6572dc5d6a05437e7065fd41c97c1f2
vachan-maker <vachan@tutanota.com>
2024-10-06 14:40:44 +0530

    Inital setup

--- app.py
+++ app.py
@@ -5,4 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 
+    return render_template('index.html')
+
+def dogs():
+    return render_template('dogs.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
